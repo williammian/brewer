7bad9660dcaa729e182c07501cdfe62d93bd1ae3
willi <willi@LAPTOP-B6ER7NR3>
2018-05-25 06:47:55 -0300

    11.1-validando-se-existe-estilo-persistido

--- src/main/java/br/com/wm/brewer/repository/Estilos.java
+++ src/main/java/br/com/wm/brewer/repository/Estilos.java
@@ -1,5 +1,7 @@
 package br.com.wm.brewer.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import br.com.wm.brewer.model.Estilo;
 @Repository
 public interface Estilos extends JpaRepository<Estilo, Long> {
 
+	public Optional<Estilo> findByNomeIgnoreCase(String nome);
+	
 }
