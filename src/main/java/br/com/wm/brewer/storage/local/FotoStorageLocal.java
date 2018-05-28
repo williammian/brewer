a7bc3cda1551e97b0a59d9466cb4ec15ea0bd0cd
William <william@multitecsistemas.com.br>
2018-05-28 10:21:48 -0300

    14.7-salvando-a-imagem-temporaria

--- src/main/java/br/com/wm/brewer/storage/local/FotoStorageLocal.java
+++ src/main/java/br/com/wm/brewer/storage/local/FotoStorageLocal.java
@@ -2,9 +2,11 @@ package br.com.wm.brewer.storage.local;
 
 import static java.nio.file.FileSystems.getDefault;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,6 +31,22 @@ public class FotoStorageLocal implements FotoStorage {
 		criarPastas();
 	}
 
+	@Override
+	public String salvarTemporariamente(MultipartFile[] files) {
+		String novoNome = null;
+		if (files != null && files.length > 0) {
+			MultipartFile arquivo = files[0];
+			novoNome = renomearArquivo(arquivo.getOriginalFilename());
+			try {
+				arquivo.transferTo(new File(this.localTemporario.toAbsolutePath().toString() + getDefault().getSeparator() + novoNome));
+			} catch (IOException e) {
+				throw new RuntimeException("Erro salvando a foto na pasta temporária", e);
+			}
+		}
+		
+		return novoNome;
+	}
+	
 	private void criarPastas() {
 		try {
 			Files.createDirectories(this.local);
@@ -38,16 +56,22 @@ public class FotoStorageLocal implements FotoStorage {
 			if (logger.isDebugEnabled()) {
 				logger.debug("Pastas criadas para salvar fotos.");
 				logger.debug("Pasta default: " + this.local.toAbsolutePath());
-				logger.debug("Pasta temporária: " + this.localTemporario.toAbsolutePath());
+				logger.debug("Pasta temporÃ¡ria: " + this.localTemporario.toAbsolutePath());
 			}
 		} catch (IOException e) {
 			throw new RuntimeException("Erro criando pasta para salvar foto", e);
 		}
 	}
-
-	@Override
-	public void salvarTemporariamente(MultipartFile[] files) {
-		System.out.println(">>>> salvando a foto temporariamente...");
+	
+	private String renomearArquivo(String nomeOriginal) {
+		String novoNome = UUID.randomUUID().toString() + "_" + nomeOriginal;
+		
+		if (logger.isDebugEnabled()) {
+			logger.debug(String.format("Nome original: %s, novo nome: %s", nomeOriginal, novoNome));
+		}
+		
+		return novoNome;
+		
 	}
 	
 }
