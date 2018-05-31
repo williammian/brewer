9a879943d138a85296cf4d84399b90025972279e
willi <willi@LAPTOP-B6ER7NR3>
2018-05-31 14:44:21 -0300

    15.7-paginacao-na-pesquisa-de-cervejas

--- src/main/java/br/com/wm/brewer/repository/helper/cerveja/CervejasQueries.java
+++ src/main/java/br/com/wm/brewer/repository/helper/cerveja/CervejasQueries.java
@@ -2,11 +2,13 @@ package br.com.wm.brewer.repository.helper.cerveja;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+
 import br.com.wm.brewer.model.Cerveja;
 import br.com.wm.brewer.repository.filter.CervejaFilter;
 
 public interface CervejasQueries {
 
-	public List<Cerveja> filtrar(CervejaFilter filtro);
+	public List<Cerveja> filtrar(CervejaFilter filtro, Pageable pageable);
 	
 }
