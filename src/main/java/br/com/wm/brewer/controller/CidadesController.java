438731fad1efce0bcf401357881b40da6b6732ad
William <william@multitecsistemas.com.br>
2018-06-06 11:22:09 -0300

    16.7-selecionando-cidades-pelo-estado-com-ajax

--- src/main/java/br/com/wm/brewer/controller/CidadesController.java
+++ src/main/java/br/com/wm/brewer/controller/CidadesController.java
@@ -1,14 +1,35 @@
 package br.com.wm.brewer.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import br.com.wm.brewer.model.Cidade;
+import br.com.wm.brewer.repository.Cidades;
 
 @Controller
+@RequestMapping("/cidades")
 public class CidadesController {
+	
+	@Autowired
+	private Cidades cidades;
 
-	@RequestMapping("/cidades/nova")
+	@RequestMapping("/nova")
 	public String nova() {
 		return "cidade/CadastroCidade";
+	}
+	
+	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<Cidade> pesquisarPorCodigoEstado(@RequestParam(name = "estado", defaultValue = "-1") Long codigoEstado) {
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {	}
+		return cidades.findByEstadoCodigo(codigoEstado);
 	}
 	
 }
