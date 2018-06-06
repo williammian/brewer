4219f90f719ace19103eb45f5479f7e9e48e037f
willi <willi@LAPTOP-B6ER7NR3>
2018-06-06 06:57:48 -0300

    16.6-aplicando-migracao-estado-e-cidade

--- src/main/java/br/com/wm/brewer/controller/ClientesController.java
+++ src/main/java/br/com/wm/brewer/controller/ClientesController.java
@@ -1,19 +1,25 @@
 package br.com.wm.brewer.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.wm.brewer.model.TipoPessoa;
+import br.com.wm.brewer.repository.Estados;
 
 @Controller
 @RequestMapping("/clientes")
 public class ClientesController {
+	
+	@Autowired
+	private Estados estados;
 
 	@RequestMapping("novo")
 	public ModelAndView novo() {
 		ModelAndView mv = new ModelAndView("cliente/CadastroCliente");
 		mv.addObject("tiposPessoa", TipoPessoa.values());
+		mv.addObject("estados", estados.findAll());
 		return mv;
 	}
 	
