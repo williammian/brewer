a70b2146067ebe9952fac3af0b076a54ec82e218
willi <willi@LAPTOP-B6ER7NR3>
2018-06-10 14:57:56 -0300

    17.3-cache-profissional-com-guava-do-google

--- src/main/java/br/com/wm/brewer/config/WebConfig.java
+++ src/main/java/br/com/wm/brewer/config/WebConfig.java
@@ -2,11 +2,12 @@ package br.com.wm.brewer.config;
 
 import java.math.BigDecimal;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.BeansException;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
@@ -30,6 +31,7 @@ import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import com.github.mxab.thymeleaf.extras.dataattribute.dialect.DataAttributeDialect;
+import com.google.common.cache.CacheBuilder;
 
 import br.com.wm.brewer.controller.CervejasController;
 import br.com.wm.brewer.controller.converter.CidadeConverter;
@@ -109,7 +111,16 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 	
 	@Bean
 	public CacheManager cacheManager() {
-		return new ConcurrentMapCacheManager();
+		//return new ConcurrentMapCacheManager(); //implementação simples
+		
+		//Cache usando Guava
+		CacheBuilder<Object, Object> cacheBuilder = CacheBuilder.newBuilder()
+				.maximumSize(3)
+				.expireAfterAccess(20, TimeUnit.SECONDS);
+		
+		GuavaCacheManager cacheManager = new GuavaCacheManager();
+		cacheManager.setCacheBuilder(cacheBuilder);
+		return cacheManager;
 	}
 
 }
