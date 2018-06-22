400bd07b7ba5dd7047acc6e1a90445080996e48c
willi <willi@LAPTOP-B6ER7NR3>
2018-06-22 09:07:56 -0300

    21.3-trabalhando-no-filtro-por-grupo

--- src/main/java/br/com/wm/brewer/repository/helper/usuario/UsuariosQueries.java
+++ src/main/java/br/com/wm/brewer/repository/helper/usuario/UsuariosQueries.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import br.com.wm.brewer.model.Usuario;
+import br.com.wm.brewer.repository.filter.UsuarioFilter;
 
 public interface UsuariosQueries {
 
 	public Optional<Usuario> porEmailEAtivo(String email);
 	
 	public List<String> permissoes(Usuario usuario);
+	
+	public List<Usuario> filtrar(UsuarioFilter filtro);
 	
 }
