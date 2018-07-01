0a45934bf1618c3a63165a95a9ee1b06d2120977
willi <willi@LAPTOP-B6ER7NR3>
2018-07-01 17:33:28 -0300

    23.17-emitindo-uma-venda

--- src/main/java/br/com/wm/brewer/service/CadastroVendaService.java
+++ src/main/java/br/com/wm/brewer/service/CadastroVendaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.wm.brewer.model.StatusVenda;
 import br.com.wm.brewer.model.Venda;
 import br.com.wm.brewer.repository.Vendas;
 
@@ -28,6 +29,12 @@ public class CadastroVendaService {
 		}
 		
 		vendas.save(venda);
+	}
+	
+	@Transactional
+	public void emitir(Venda venda) {
+		venda.setStatus(StatusVenda.EMITIDA);
+		salvar(venda);
 	}
 	
 }
