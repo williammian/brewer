da5cb5e7cb8c19ef5d901c3bff83b4deba7a4d9a
William <william@multitecsistemas.com.br>
2018-07-02 08:59:48 -0300

    23.18-desafio-implementando-a-pesquisa-das-vendas

--- src/main/java/br/com/wm/brewer/repository/Vendas.java
+++ src/main/java/br/com/wm/brewer/repository/Vendas.java
@@ -3,7 +3,8 @@ package br.com.wm.brewer.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.wm.brewer.model.Venda;
+import br.com.wm.brewer.repository.helper.venda.VendasQueries;
 
-public interface Vendas extends JpaRepository<Venda, Long> {
+public interface Vendas extends JpaRepository<Venda, Long>, VendasQueries {
 
 }
---
+++ src/main/java/br/com/wm/brewer/repository/helper/venda/VendasQueries.java
@@ -0,0 +1,13 @@
+package br.com.wm.brewer.repository.helper.venda;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import br.com.wm.brewer.model.Venda;
+import br.com.wm.brewer.repository.filter.VendaFilter;
+
+public interface VendasQueries {
+
+	public Page<Venda> filtrar(VendaFilter filtro, Pageable pageable);
+	
+}
