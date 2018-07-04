baa7c6c93aaaa4f5e508c9a5201caefa3819e7e8
William <william@multitecsistemas.com.br>
2018-07-04 14:50:26 -0300

    25.1-implementando-a-exclusao-de-cerveja

--- src/main/java/br/com/wm/brewer/storage/local/FotoStorageLocal.java
+++ src/main/java/br/com/wm/brewer/storage/local/FotoStorageLocal.java
@@ -19,6 +19,7 @@ import net.coobird.thumbnailator.name.Rename;
 public class FotoStorageLocal implements FotoStorage {
 
 	private static final Logger logger = LoggerFactory.getLogger(FotoStorageLocal.class);
+	private static final String THUMBNAIL_PREFIX = "thumbnail.";
 	
 	private Path local;
 	private Path localTemporario;
@@ -85,7 +86,18 @@ public class FotoStorageLocal implements FotoStorage {
 	
 	@Override
 	public byte[] recuperarThumbnail(String fotoCerveja) {
-		return recuperar("thumbnail." + fotoCerveja);
+		return recuperar(THUMBNAIL_PREFIX + fotoCerveja);
+	}
+	
+	@Override
+	public void excluir(String foto) {
+		try {
+			Files.deleteIfExists(this.local.resolve(foto));
+			Files.deleteIfExists(this.local.resolve(THUMBNAIL_PREFIX + foto));
+		} catch (IOException e) {
+			logger.warn(String.format("Erro apagando foto '%s'. Mensagem: %s", foto, e.getMessage()));
+		}
+		
 	}
 	
 	private void criarPastas() {
