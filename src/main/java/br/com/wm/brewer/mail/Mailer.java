f5a5bfed2a4937f5d5159625e71d408255bb2a3b
willi <willi@LAPTOP-B6ER7NR3>
2018-07-03 23:13:37 -0300

    24.5-criando-template-para-email

--- src/main/java/br/com/wm/brewer/mail/Mailer.java
+++ src/main/java/br/com/wm/brewer/mail/Mailer.java
@@ -17,7 +17,7 @@ public class Mailer {
 	@Async
 	public void enviar(Venda venda) {
 		SimpleMailMessage mensagem = new SimpleMailMessage();
-		mensagem.setFrom("william_mian@yahoo.com.br");
+		mensagem.setFrom("williammian@gmail.com");
 		mensagem.setTo(venda.getCliente().getEmail());
 		mensagem.setSubject("Venda Efetuada");
 		mensagem.setText("Obrigado, sua venda foi processada!");
